3f74246a47f9474670de8f09b8ed7d3cf152ef2c
handwoong <handwoong@gmail.com>
2024-12-10 17:38:52 +0900

    feat: 다국어 지원을 위한 메시지소스 설정 (#9)

---
+++ src/main/java/com/everyonewaiter/common/ExceptionMessageFormatter.java
@@ -0,0 +1,31 @@
+package com.everyonewaiter.common;
+
+import java.util.List;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class ExceptionMessageFormatter {
+
+	private static final String EXCEPTION_MESSAGE_FORMAT = "%s {%s} [%s]";
+
+	public static String format(String message, Object argument, String argumentDetail) {
+		return format(message, List.of(argument), List.of(argumentDetail));
+	}
+
+	public static String format(String message, List<?> arguments) {
+		List<String> argumentDetails = arguments.stream()
+			.map(argument -> argument.getClass().getSimpleName())
+			.toList();
+		return format(message, arguments, argumentDetails);
+	}
+
+	public static String format(String message, List<?> arguments, List<String> argumentDetails) {
+		return EXCEPTION_MESSAGE_FORMAT.formatted(
+			message,
+			String.join(", ", arguments.stream().map(Object::toString).toList()),
+			String.join(", ", argumentDetails)
+		);
+	}
+}
